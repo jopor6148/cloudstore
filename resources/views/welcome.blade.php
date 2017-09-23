108241f2f9fd71402b31a5d6dc7f1021b12b15ee
jopor6148 <jopor6148@gmail.com>
2017-09-23 00:27:44 -0500

    prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel mio
                 </div>
 
                 <div class="links">
